5d42eb717e196058992c75d9b44f1203fa7876d4
gianni999 <gianniliotti2000@gmail.com>
2022-06-13 18:59:20 -0300

    Update Gastos.js

--- Gastos.js
+++ Gastos.js
@@ -3,14 +3,14 @@ let suma=0;
 let cut=0;
 
 function add() {
-    let name = $("#userName").val();
-    let money = $("#userMoney").val();
-    cont +=1;
+    let name=$("#userName").val();
+    let money=$("#userMoney").val();
+    cont+=1;
 
     distribute(name, money, cont);
 
-    let nametoshow= `<h5> ${name}: </h5>`
-    let moneytoshow= `<h5> $ ${money}: </h5>`
+    let nametoshow=`<h5> ${name}: </h5>`
+    let moneytoshow=`<h5> $ ${money}: </h5>`
 
     $("#historyName").append(nametoshow);
     $("#historyMoney").append(moneytoshow);
@@ -20,6 +20,6 @@ function distribute(_person, spent, c) {
     suma+= parseInt(spent);
     $("#result").text("$" + suma);
 
-    cut = suma / c;
+    cut=suma / c;
     $("#cut").text("$" + (Math.round(cut)));
 }
